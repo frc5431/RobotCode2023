c7a2a7f3231558249c4288c833a9aa0114a244e1
Rubyboat1207 <rudy@rudysoliz.com>
2023-02-07 17:32:35 -0600

    Segment 1

--- src/main/java/frc/robot/util/InverseKinematicsSolver.java
+++ src/main/java/frc/robot/util/InverseKinematicsSolver.java
@@ -14,4 +14,18 @@ public class InverseKinematicsSolver {
 
     // Cos triangle rule: A^2 = B^2 + C^2 - 2BC * cos(a)
     // Isolate b: 
+
+    double solveSegment1Angle(double q2, Translation2d goal) {
+        double p1 = Math.atan2(goal.getY(), goal.getX());
+        double p2 = Math.atan2(l2 * Math.sin(q2), l1 + l2 * Math.cos(q2));
+
+        return p1 - p2;
+    }
+
+    PresetPosition solveForPosition(Translation2d goal) {
+        double outerAngle = 0.; //Replace once implemented
+        double innerAngle = solveSegment1Angle(outerAngle, goal);
+
+        return new PresetPosition(outerAngle, innerAngle, 0);
+    }
 }
