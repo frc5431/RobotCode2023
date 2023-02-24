c3b7b0a1eeee198dc6a274a45eafbacb20251143
Rubyboat <rudy@rudysoliz.com>
2023-02-24 00:17:59 -0600

    choose superposition collapse state

--- src/main/java/frc/robot/util/KinematicsSolver.java
+++ src/main/java/frc/robot/util/KinematicsSolver.java
@@ -4,8 +4,13 @@ import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Translation2d;
 
 public class KinematicsSolver {
+    public static boolean PREFER_BOTTOM_POSSIBILITY = true;
+    public static boolean PREFER_ABOVE_POSSIBILITY = false;
+
+
     private double l1; // Length of first arm segment
     private double l2; // Length of second arm segment
+    public boolean SuperpositionCollapsePreferance = PREFER_BOTTOM_POSSIBILITY;
 
     public KinematicsSolver(double armSegment1Length, double armSegment2Length) {
         this.l1 = armSegment1Length;
@@ -19,18 +24,18 @@ public class KinematicsSolver {
         double p1 = Math.pow(goal.getX(),2) + Math.pow(goal.getY(),2) - Math.pow(l1,2) - Math.pow(l2,2);
         double p2 = 2*l1*l2;
 
-        return Math.acos(p1/p2);
+        return (SuperpositionCollapsePreferance == PREFER_ABOVE_POSSIBILITY ? -1 : 1) * Math.acos(p1/p2);
     }
 
     private double solveSegment1Angle(double q2, Translation2d goal) {
         double p1 = Math.atan2(goal.getY(), goal.getX());
         double p2 = Math.atan2(l2 * Math.sin(q2), l1 + l2 * Math.cos(q2));
 
-        return p1 - p2;
+        return (SuperpositionCollapsePreferance == PREFER_ABOVE_POSSIBILITY ? p1 + p2 : p1 - p2);
     }
 
     public Pair<Double, Double> posToAngles(Translation2d goal) {
-        double q2 = solveSegment2Angle(goal); //Replace once implemented
+        double q2 = solveSegment2Angle(goal);
         double q1 = solveSegment1Angle(q2, goal);
 
         return Pair.of(q1 + Math.PI/2, -q2);
