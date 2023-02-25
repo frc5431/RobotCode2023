b1b40c35c55c721e35b06e024366911f9b03eba4
Natalie <64873273+DoubleSnapps@users.noreply.github.com>
2023-02-25 10:18:47 -0600

    calvin fix

--- src/main/java/frc/robot/commands/AutoAligner.java
+++ src/main/java/frc/robot/commands/AutoAligner.java
@@ -6,31 +6,32 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivebase;
 
-
-public class AutoAligner extends CommandBase{
+public class AutoAligner extends CommandBase {
     public final Pigeon2 pigy;
     public final Drivebase drivebase;
     public PIDController pid = new PIDController(0, 0, 0);
     public ChassisSpeeds cs = new ChassisSpeeds(0, 0, 0);
-    
-    public AutoAligner(Drivebase drivebase){
-           this.drivebase = drivebase;
-           this.pigy = drivebase.getGyro();
-           pid.setTolerance(2.5);
-    }       
+
+    public AutoAligner(Drivebase drivebase) {
+        this.drivebase = drivebase;
+        this.pigy = drivebase.getGyro();
+        pid.setTolerance(2.5);
+    }
 
     @Override
-    public void initialize(){
+    public void initialize() {
         boolean robotIsBackwards = Math.abs(180 - pigy.getYaw()) <= 1;
         double direction = robotIsBackwards ? 1 : -1; // TODO direction is not used? // yeah i forgot why
     }
 
     @Override
-    public void execute(){
-        cs.vxMetersPerSecond = pid.calculate(pigy.getPitch(), 0);/    =rivebase.drive(cs);    }8/* */
+    public void execute() {
+        cs.vxMetersPerSecond = pid.calculate(pigy.getPitch(), 0);
+        drivebase.drive(cs);
+    }
 
     @Override
-    public boolean isFinished(){
+    public boolean isFinished() {
         return pid.atSetpoint();
     }
 
