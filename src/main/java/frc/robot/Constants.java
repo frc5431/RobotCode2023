4d2a4291c3d33bfacb591188ade0d611ba4c4168
test <60162283+5431-programming@users.noreply.github.com>
2023-04-13 18:49:54 -0500

    new swerve offsets

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -68,22 +68,22 @@ public class Constants {
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 2;
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 1;
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 9;
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(293.906); // 264.375
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(291.888); // 293.906
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 8;
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 7;
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 12;
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(105.381); // 108.281
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(103.975); // 105.381
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4;
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 3;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 10;
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(111.533); // 330.645
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(110.654); // 111.533
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 6;
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 5;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11;
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(53.701); // 115.664
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(52.910); // 53.701
 
     //#region Auto Constants
     public static final PathConstraints PATH_CONSTRAINTS = new PathConstraints(4, 3);
